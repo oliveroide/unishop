7a2564ca81814380291869fe7e2aa881a8ee3c55
FranciscoJuarez1108 <fra.jcabrera@gmail.com>
2025-05-21 00:34:31 -0700

    hice el carrito

--- core/views.py
+++ core/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+ from django.shortcuts import render, redirect
+  from .cart import Cart
 
-def home(request):
-    return render(request, 'products/list.html') 
-def product_list(request):
-    return render(request, 'products/detail.html')
+    def home(request):
+        return render(request, 'products/list.html')
+
+    def product_list(request):
+        return render(request, 'products/detail.html')
+
+    def add_to_cart(request, product_id):
+        cart = Cart(request)
+        cart.add(product_id)
+        return redirect('product_list')
+
+    def remove_from_cart(request, product_id):
+        cart = Cart(request)
+        cart.remove(product_id)
+        return redirect('cart_detail')
+
+    def cart_detail(request):
+        cart = Cart(request)
+        return render(request, 'cart/detail.html', {'cart': cart})
